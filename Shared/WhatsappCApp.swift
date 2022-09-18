0bf60409b056d06b6caa47b3f1c29f949902d693
Andres Gonzalez <32281805+andres2392@users.noreply.github.com>
2022-09-18 19:22:03 -0400

    Initial Commit

---
+++ Shared/WhatsappCApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhatsappCApp.swift
+//  Shared
+//
+//  Created by Andres Gonzalez on 9/18/22.
+//
+
+import SwiftUI
+
+@main
+struct WhatsappCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
